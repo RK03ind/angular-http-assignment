493df3b4a3d6beca317f16b101ef2ddc604e8f6e
RK03ind <gamitech95@gmail.com>
2024-09-07 20:23:49 +0530

    ADD: Pagination feature added

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../data.service';
 
@@ -13,12 +13,46 @@ export class PostListComponent {
   public posts: any[] = [];
   public errorMsg: string = '';
 
+  public currentPage: number = 1;
+  public itemsPerPage: number = 12;
+  public totalItems: number = 0;
+
+  public pagedItems: any[] = [];
+
+  @ViewChild('pagination', { static: false }) paginationElement!: ElementRef;
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.dataService.getData().subscribe({
-      next: (data) => (this.posts = data),
+      next: (data) => {
+        this.posts = data;
+        this.totalItems = data.length;
+        this.updatePagedItems();
+      },
       error: (error) => (this.errorMsg = error),
+    });
+  }
+
+  updatePagedItems(): void {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.pagedItems = this.posts.slice(start, end);
+  }
+
+  goToPage(page: number): void {
+    if (page > 0 && page <= this.getTotalPages()) {
+      this.currentPage = page;
+      this.updatePagedItems();
+    }
+  }
+
+  getTotalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  scrollToPagination(): void {
+    this.paginationElement.nativeElement.scrollIntoView({
+      behavior: 'smooth',
     });
   }
 }
